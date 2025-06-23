efd81da5a4534844c4d450b151ef21f76ba6d0e4
p-k-pavan <pkpavan2003@email.com>
2025-06-23 08:20:12 +0530

    Hero section update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -23,7 +23,8 @@ const Hero = () => {
           fill="blue"
         />
 
-        <div className="absolute h-full w-full dark:bg-black bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2]">
+        <div className="absolute h-full w-full dark:bg-black bg-white dark:bg-grid-white/[0.15] bg-grid-black/[0.25]">
+
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
       </div>
